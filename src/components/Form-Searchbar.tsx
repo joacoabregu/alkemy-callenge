3041b207f7c2b70b1e2e647e354c1a2aa7a67915
Joaquin Abregu <abregujoaquin@gmail.com>
2021-09-26 19:50:02 -0300

    initial commit

---
+++ src/components/Form-Searchbar.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import axios from "axios";
+import { useFormik } from "formik";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+interface heroSearchResponse {
+  response: string;
+  "results-for": string;
+  results: [];
+}
+
+type FormSearchBarProps = {
+  setter: React.Dispatch<React.SetStateAction<[] | undefined>>;
+};
+
+const validate = (values: { search?: string }) => {
+  const errors: { search?: string } = {};
+
+  if (!values.search) {
+    errors.search = "Required";
+  }
+
+  return errors;
+};
+
+export default function FormSearchBar({ setter }: FormSearchBarProps) {
+  const formik = useFormik({
+    initialValues: {
+      search: "",
+    },
+    validate,
+    onSubmit: (value) => {
+      
+      let url = "/search/" + value.search;
+
+      axios
+        .get<heroSearchResponse>(url)
+        .then((response) => {
+          let { results } = response.data;
+          setter(results);
+          console.log(results);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  });
+
+  return (
+    <Form onSubmit={formik.handleSubmit}>
+      <Form.Group className="mb-3" controlId="search">
+        <Form.Control
+          name="search"
+          type="search"
+          placeholder="Search"
+          className="mr-2"
+          aria-label="Search"
+          value={formik.values.search}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+        />
+        {formik.touched.search && formik.errors.search ? (
+          <Form.Text>{formik.errors.search}</Form.Text>
+        ) : null}
+      </Form.Group>
+      <Button variant="outline-success" type="submit">
+        Search
+      </Button>
+    </Form>
+  );
+}
