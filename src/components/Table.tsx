a364a510e1adfb55a5f0056f6cbbafa187bdb10c
Joaquin Abregu <abregujoaquin@gmail.com>
2021-09-28 14:06:42 -0300

    initial commit

---
+++ src/components/Table.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Table from "react-bootstrap/Table";
+import { TableProps } from "../types/types";
+
+export default function StatsTable({ stats }: TableProps) {
+  let tableData = [
+    ["Inteligencia", stats.intelligence],
+    ["Fuerza", stats.strength],
+    ["Velocidad", stats.speed],
+    ["Poder", stats.power],
+    ["Resistencia", stats.durability],
+    ["Combate", stats.combat],
+  ];
+  let orderData = tableData.sort(function (a, b) {
+    var valueA, valueB;
+
+    valueA = parseInt(a[1]); 
+    valueB = parseInt(b[1]);
+    if (valueA > valueB) {
+      return -1;
+    } else if (valueA < valueB) {
+      return 1;
+    }
+    return 0;
+  });
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Power</th>
+          <th>Puntaje</th>
+        </tr>
+      </thead>
+      <tbody>
+        {orderData.map((data, index) => {
+          return (
+            <tr key={index}>
+              <td> {data[0]} </td>
+              <td> {data[1]} </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
+}
