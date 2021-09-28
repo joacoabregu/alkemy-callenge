51e70467c8d45d68d0d060ba2b9a3920f540c27d
Joaquin Abregu <abregujoaquin@gmail.com>
2021-09-28 14:07:11 -0300

    LinkContainer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default function Header() {
   return (
     <Nav defaultActiveKey="/home" as="ul">
       <Nav.Item as="li">
-        <Nav.Link href="/home">Home</Nav.Link>
+        <LinkContainer to="/home">
+          <Nav.Link>Home</Nav.Link>
+        </LinkContainer>
       </Nav.Item>
       <Nav.Item as="li">
-        <Nav.Link eventKey="link-1" href="/login">
-          Login
-        </Nav.Link>
+        <LinkContainer to="/login">
+          <Nav.Link eventKey="link-1">Login</Nav.Link>
+        </LinkContainer>
       </Nav.Item>
       <Nav.Item as="li">
         <Nav.Link eventKey="link-2">Link</Nav.Link>
