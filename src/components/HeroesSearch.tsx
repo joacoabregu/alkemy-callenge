21e5f2d50b70302a7db93e1d8368a6b1cb8d3a1f
Joaquin Abregu <abregujoaquin@gmail.com>
2021-09-30 15:29:38 -0300

    style grid

--- src/components/HeroesSearch.tsx
+++ src/components/HeroesSearch.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import FormSearchBar from "./Form-Searchbar";
@@ -9,15 +8,10 @@ export default function Login() {
   let [heroes, setHeroes] = useState<[]>();
 
   return (
-    <Container fluid="sm">
-      <Row>
-        <Col>
-          <FormSearchBar setter={setHeroes} />
-        </Col>
-      </Row>
-      <Row>
-        <Col>{heroes ? <HeroesCard heroes={heroes} /> : null}</Col>
-      </Row>
-    </Container>
+    <>
+      <FormSearchBar setter={setHeroes}  />
+
+      {heroes ? <HeroesCard heroes={heroes} /> : null}
+    </>
   );
 }
