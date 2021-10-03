72ae5ebcf4e1f5a151a0f8a61254a772e59d1645
Joaquin Abregu <abregujoaquin@gmail.com>
2021-10-03 11:50:51 -0300

    searchbar props

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,12 +1,24 @@
 import { Hero } from "../types/interfaces";
+import { Stats } from "../types/interfaces";
+
 
 export type Heroes = {
   heroes: Hero[];
+  
+};
+
+export type HeroesCardsProps = {
+  heroes: Hero[];
+  setter: React.Dispatch<React.SetStateAction<[] | undefined>>;
 };
 
 export type HeroProps = {
   hero: Hero;
 };
+
+export type TableProps = {
+  stats: Stats;
+}
 
 export type FormSearchBarProps = {
   setter: React.Dispatch<React.SetStateAction<[] | undefined>>;
