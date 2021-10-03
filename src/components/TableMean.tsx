fd40ee294ae35d7b11b084ff48b273e343958ff8
Joaquin Abregu <abregujoaquin@gmail.com>
2021-10-03 11:30:42 -0300

    helper functions

--- src/components/TableMean.tsx
+++ src/components/TableMean.tsx
@@ -4,44 +4,17 @@ import Row from "react-bootstrap/esm/Row";
 import Table from "react-bootstrap/Table";
 import { useSelector } from "react-redux";
 import { RootState } from "../state/store";
-import { Appearance, Hero } from "../types/interfaces";
+import { Appearance } from "../types/interfaces";
+import { teamMean, sumHeroesAppearance } from "../helpers/functions";
 
-type AppearanceKeys = keyof Appearance;
 export default function TableMean() {
   let team = useSelector((state: RootState) => state.team.team);
   let [mean, setMean] = useState<Appearance>();
 
   useEffect(() => {
-    let baseMean = {
-      weight: "0",
-      height: "0",
-    };
-    const sumObjectsByKey = (objs: Hero[]) => {
-      const res = objs.reduce((a, b) => {
-        let power = b.appearance;
-        let weight = power.weight[1];
-        let height = power.height[1];
-        let base = { weight, height };
-        let k: AppearanceKeys;
-        for (k in base) {
-          let result: number = parseInt(a[k]) + parseInt(base[k]);
-          a[k] = result.toString();
-        }
-        return a;
-      }, baseMean);
-      return res;
-    };
-
-    let meanArr = sumObjectsByKey(team);
+    let meanArr = sumHeroesAppearance(team);
     if (team.length) {
-      let meanWeight = (parseInt(meanArr.weight) / team.length)
-        .toFixed(2)
-        .replace(/\.00$/, "");
-      let meanHeight = (parseInt(meanArr.height) / team.length)
-        .toFixed(2)
-        .replace(/\.00$/, "");
-      let actualMean = { weight: meanWeight, height: meanHeight };
-      setMean(actualMean);
+      setMean(teamMean(meanArr.weight, meanArr.height, team.length));
     } else {
       setMean({ weight: "0", height: "0" });
     }
