a5efd82226a18c638ec1ff39c1069cfd9627eb1a
Joaquin Abregu <abregujoaquin@gmail.com>
2021-10-03 17:40:06 -0300

    removed helper functions

--- src/components/TableMean.tsx
+++ src/components/TableMean.tsx
@@ -4,16 +4,36 @@ import Row from "react-bootstrap/Row";
 import Table from "react-bootstrap/Table";
 import { useSelector } from "react-redux";
 import { RootState } from "../state/store";
-import { Appearance } from "../types/interfaces";
-import { teamMean, sumHeroesAppearance } from "../helpers/functions";
-
+import { Appearance, Hero } from "../types/interfaces";
+import { teamMean} from "../helpers/functions";
+type AppearanceKeys = keyof Appearance;
 export default function TableMean() {
   let team = useSelector((state: RootState) => state.team.team);
   let [mean, setMean] = useState<Appearance>();
 
   useEffect(() => {
-    let meanArr = sumHeroesAppearance(team);
+    let baseMean = {
+      weight: "0",
+      height: "0",
+    };
+    const sumObjectsByKey = (objs: Hero[]) => {
+      const res = objs.reduce((a, b) => {
+        let power = b.appearance;
+        let weight = power.weight[1];
+        let height = power.height[1];
+        let base = { weight, height };
+        let k: AppearanceKeys;
+        for (k in base) {
+          let result: number = parseInt(a[k]) + parseInt(base[k]);
+          a[k] = result.toString();
+        }
+        return a;
+      }, baseMean);
+      return res;
+    };
+    let meanArr = sumObjectsByKey(team);
     if (team.length) {
+      
       setMean(teamMean(meanArr.weight, meanArr.height, team.length));
     } else {
       setMean({ weight: "0", height: "0" });
--- src/components/teamStats.tsx
+++ src/components/teamStats.tsx
@@ -1,17 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../state/store";
-import { Stats } from "../types/interfaces";
+import { Hero, Stats } from "../types/interfaces";
 import StatsTable from "./Table";
-import { sumHeroesPowerStats } from "../helpers/functions";
+
+type StatsKeys = keyof Stats;
 
 export default function TeamStatsTable() {
   let team = useSelector((state: RootState) => state.team.team);
   let [stats, setStats] = useState<Stats>();
 
   useEffect(() => {
-    let powerstats = sumHeroesPowerStats(team);
-    setStats(powerstats);
+        let stats = {
+          intelligence: "0",
+          strength: "0",
+          speed: "0",
+          durability: "0",
+          power: "0",
+          combat: "0",
+        };
+        const sumObjectsByKey = (objs: Hero[]) => {
+          const res = objs.reduce((a, b) => {
+            let power = b.powerstats;
+            let k: StatsKeys;
+            for (k in power) {
+              
+              let heroValue = parseInt(power[k]) || 0;
+
+              let result: number = parseInt(a[k]) + heroValue;
+              a[k] = result.toString();
+            }
+            return a;
+          }, stats);
+          return res;
+        };
+        let powerstats = sumObjectsByKey(team);
+        setStats(powerstats);
   }, [team]);
   return <>{stats ? <StatsTable stats={stats} /> : null}</>;
 }
